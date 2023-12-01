9696ec38309ab8939c5d16be88b534a3b9aca577
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-01 11:54:52 +0600

    Update code

---
+++ code/web_requests.py
@@ -0,0 +1,25 @@
+
+
+   import requests
+   import time
+   import logging
+   from configparser import ConfigParser
+
+   config = ConfigParser()
+   config.read('config.ini')
+
+   logging.basicConfig(level=logging.INFO)
+   logger = logging.getLogger(__name__)
+
+   def make_realistic_request(url):
+       try:
+           time.sleep(1)
+           headers = {"User-Agent": config.get('Request', 'User-Agent')}
+           response = requests.get(url, headers=headers, timeout=10)
+           response.raise_for_status()
+           return response.content
+       except requests.exceptions.Timeout:
+           logger.error(f"Request to {url} timed out.")
+       except requests.exceptions.RequestException as e:
+           logger.error(f"Error making request to {url}: {e}")
+       return None
