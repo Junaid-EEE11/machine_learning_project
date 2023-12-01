4cbc77f19d816b2ae9c121bcb52d59b9d628f9b1
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-01 12:24:48 +0600

    Update code

---
+++ code/machine_model.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+import joblib
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
+from pandas import read_csv
+
+
+iris = datasets.load_iris()
+df_iris = pd.DataFrame(iris.data, columns = iris.feature_names)
+df_iris['class'] = iris.target
+df_iris.head()
+df_iris.to_csv('iris.csv')
+print(df_iris.shape)
+X = df_iris.drop('class',axis = 1)
+y = df_iris['class']
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
+model_1= LogisticRegression()
+model_1.fit(X_train, Y_train)
+predictions = model_1.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+print("-------------model_1 completed----------------------------")
+
+model_2 = DecisionTreeClassifier()
+model_2.fit(X_train, Y_train)
+predictions = model_2.predict(X_validation)
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+print("---------------------model_2 completed--------------------")
+
+model_3 = DecisionTreeClassifier()
+model_3.fit(X_train, Y_train)
+predictions = model_3.predict(X_validation)
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+print("--------------------model_3 completed---------------------")
+
+model_4 = KNeighborsClassifier()
+model_4.fit(X_train, Y_train)
+predictions = model_4.predict(X_validation)
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+print("----------------model_4 completed-------------------------")
+
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+.  model.save('{%f}.joblib'.name)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+# Compare Algorithms
+plt.boxplot(results, labels=names)
+plt.title('Algorithm Comparison')
+plt.show()
+print("-------------model_comparison completed-------------------------")
