964430e184c11f00bb2a2604b6360d2a9b6cead3
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-01 12:10:04 +0600

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,28 +1,28 @@
 from concurrent.futures import ThreadPoolExecutor
-   from web_requests import make_realistic_request
-   from scraping import scrape_data_with_bs4, scrape_data_with_selenium
-   from data_handling import save_data_to_hdf5
-   import logging
+from web_requests import make_realistic_request
+from scraping import scrape_data_with_bs4, scrape_data_with_selenium
+from data_handling import save_data_to_hdf5
+import logging
 
-   logging.basicConfig(level=logging.INFO)
-   logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-   def scrape_and_save_parallel(alibaba_urls):
-       data_to_save = {}
+def scrape_and_save_parallel(alibaba_urls):
+    data_to_save = {}
 
-       for i, url in enumerate(alibaba_urls):
-           bs4_data = scrape_data_with_bs4(url)
-           if bs4_data:
-               data_to_save[f'bs4_data_{i}'] = bs4_data
+    for i, url in enumerate(alibaba_urls):
+        bs4_data = scrape_data_with_bs4(url)
+        if bs4_data:
+           data_to_save[f'bs4_data_{i}'] = bs4_data
 
-       for i, url in enumerate(alibaba_urls):
-           selenium_data = scrape_data_with_selenium(url)
-           if selenium_data:
-               data_to_save[f'selenium_data_{i}'] = selenium_data
+    for i, url in enumerate(alibaba_urls):
+        selenium_data = scrape_data_with_selenium(url)
+        if selenium_data:
+             data_to_save[f'selenium_data_{i}'] = selenium_data
 
        save_data_to_hdf5(data_to_save, config.get('General', 'marketdata'))
 
-   if __name__ == "__main__":
+if __name__ == "__main__":
        alibaba_urls = ["https://www.alibaba.com/"]
        scrape_and_save_parallel(alibaba_urls)
 
