f29e0767f2da39692516a3fd0493677bb3e79799
Junaid Hossain <148847367+Junaid-EEE11@users.noreply.github.com>
2023-12-01 12:00:24 +0600

    Update data_handling.py

--- code/data_handling.py
+++ code/data_handling.py
@@ -3,14 +3,13 @@ import logging
 from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.ini')
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-   logging.basicConfig(level=logging.INFO)
-   logger = logging.getLogger(__name__)
-
-   def save_data_to_hdf5(data, filename):
-       try:
-           with h5py.File(filename, 'w') as hdf_file:
-               for key, value in data.items():
-                   hdf_file.create_dataset(key, data=value)
-       except Exception as e:
-           logger.error(f"Error saving data to HDF5: {e}")
+def save_data_to_hdf5(data, filename):
+    try:
+        with h5py.File(filename, 'w+') as hdf_file:
+        for key, value in data.items():
+             hdf_file.create_dataset(key, data=value)
+    except Exception as e:
+       logger.error(f"Error saving data to HDF5: {e}")
